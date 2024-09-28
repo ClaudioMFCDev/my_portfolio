0fe1584c25542e1235d262019803ca5c6f0b4bf1
Claudio <ccmf.dev@gmail.com>
2024-09-28 16:53:56 -0300

    fix: update cads education

--- src/components/Educa-com.tsx
+++ src/components/Educa-com.tsx
@@ -34,16 +34,16 @@ const educationData: EducationEntry[] = [
 const Educacion: React.FC = () => {
   return (
     <section id="education" className="container mx-auto mt-6">
-      <h3 className=" text-4xl sm:text-5xl text-center text-deepblue-500">Education</h3>
-      <div className="cards mb-6 flex justify-center items-center flex-col">
+      <h3 className="mb-2 text-4xl sm:text-5xl text-center text-deepblue-500">Education</h3>
+      <div className="cards mb-6 flex flex-wrap gap-2 sm:gap-4 justify-center items-center">
         {educationData.map( (entry, index) => (
-          <div key={index} className="mt-4 bg-deepblue-500 rounded border-2 w-1/2">
-            <div className="h-8 w-28 my-2 -ml-2 content-center absolute border-2 rounded-r-xl my-col">
-              <h3 className="mx-4 text-lg text-deepblue-200 font-black">{entry.year}</h3>
+          <div key={index} className=" bg-deepblue-500 rounded border-2 w-1/2 max-w-80 min-w-72 max-lg:max-h-36 shadow-xl shadow-deepblue-300">
+            <div className="h-8 w-28 mt-0.5 -ml-1.5 content-center absolute border-2 rounded-r-xl my-col bg-deepblue-400">
+              <h3 className="mx-4 text-base sm:text-lg text-white font-black">{entry.year}</h3>
             </div>
-            <div className="pl-4 h-40 mx-4 content-center text-white">
-              <h3 className="my-4 text-2xl font-bold">{entry.institution}</h3>
-              <p className="italic text-xl font-semibold">{entry.description}</p>
+            <div className="pb-0 h-40 mx-4 content-center text-white">
+              <h3 className="my-1 text-lg sm:text-xl font-bold">{entry.institution}</h3>
+              <p className="italic text-sm sm:text-base text-deepblue-200 font-semibold">{entry.description}</p>
             </div>
           </div>
         ) )}
